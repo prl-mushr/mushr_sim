8f978566c680550acaaf8d69ef794ae0a0a37d2a
Matthew <Rockett8855@users.noreply.github.com>
2019-08-20 18:14:09 -0700

    Add linter and licence (#6)
    * Add linter and licence stuff
    
    * Update package
    
    [skip ci]
    
    * Fix linter stuff

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2019, The Personal Robotics Lab, The MuSHR Team, The Contributors of MuSHR
+# License: BSD 3-Clause. See LICENSE.md file in root directory.
+
 import numpy as np
 import tf
 import tf.transformations
