d3fde0cf5c1cb69e3129e4a5a46fa391e3bbaf71
Brian Hou <brian.hou@live.com>
2021-05-02 18:38:23 -0700

    Add __future__ imports.

--- src/mushr_sim/fake_urg.py
+++ src/mushr_sim/fake_urg.py
@@ -3,6 +3,8 @@
 # Copyright (c) 2019, The Personal Robotics Lab, The MuSHR Team, The Contributors of MuSHR
 # License: BSD 3-Clause. See LICENSE.md file in root directory.
 
+from __future__ import absolute_import, division, print_function
+
 import numpy as np
 import range_libc
 import rospy
