a426218828de4418b5972cfbad10ed6c9c9a4877
Abhinav Diddee <adiddee@live.com>
2021-07-12 14:47:17 -0700

    rewrote multi car simulation to be parametric

---
+++ src/multi_car.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+
+import rospy
+import roslaunch
+import sys
+uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+roslaunch.configure_logging(uuid)
+
+#number of cars
+n=2
+
+launches = []
+mapserver = 1
+colors = ['','-green-black','-red-white','-teal-purple','-purple-gold']
+for x in range(n):
+    file_name='teleop.launch'
+    cli_args = ['mushr_sim', file_name, 'car_name:=car'+str(x),  'racecar_color:='+ colors[x%5], 'map_server:='+ str(mapserver)]
+    roslaunch_file = roslaunch.rlutil.resolve_launch_arguments(cli_args)[0]
+    roslaunch_args = cli_args[2:]
+    launches.append((roslaunch_file, roslaunch_args))
+    if(mapserver == 1):
+        mapserver = 0
+parent = roslaunch.parent.ROSLaunchParent(uuid, launches)
+parent.start()
+while not rospy.is_shutdown():
+    rospy.sleep(1)
